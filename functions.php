45dc4057cc16b23a671e4d7200b0a8c4537a72ca
Mohammed Akram Uddin <94456647+MohammedAkramU7@users.noreply.github.com>
2024-07-30 23:49:26 +0530

    t

--- functions.php
+++ functions.php
@@ -12,7 +12,3 @@ function my_basic_theme_scripts() {
     wp_enqueue_style( 'my-basic-theme-style', get_stylesheet_uri() );
 }
 add_action( 'wp_enqueue_scripts', 'my_basic_theme_scripts' );
-<<<<<<< HEAD
-
-=======
->>>>>>> parent of 039bcb5 (Add Github support)
