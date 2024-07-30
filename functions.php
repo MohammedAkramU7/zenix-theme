30891eed9bca3eeb828cbd8525a0d7970ef679f6
Mohammed Akram Uddin <94456647+MohammedAkramU7@users.noreply.github.com>
2024-07-30 22:15:39 +0530

    Add Basic code
    Add content

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+function my_basic_theme_setup() {
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'post-thumbnails' );
+    register_nav_menus( array(
+        'menu-1' => esc_html__( 'Primary', 'my-basic-theme' ),
+    ) );
+}
+add_action( 'after_setup_theme', 'my_basic_theme_setup' );
+
+function my_basic_theme_scripts() {
+    wp_enqueue_style( 'my-basic-theme-style', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'my_basic_theme_scripts' );
