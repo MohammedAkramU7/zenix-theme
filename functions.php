7f31f472f0695de41b055d5e7a2c03fda7283838
Mohammed Akram Uddin <94456647+MohammedAkramU7@users.noreply.github.com>
2024-07-31 00:07:47 +0530

    t

--- functions.php
+++ functions.php
@@ -12,3 +12,13 @@ function my_basic_theme_scripts() {
     wp_enqueue_style( 'my-basic-theme-style', get_stylesheet_uri() );
 }
 add_action( 'wp_enqueue_scripts', 'my_basic_theme_scripts' );
+
+/** here is the Github updater */
+require 'plugin-update-checker/plugin-update-checker.php';
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+	'https://github.com/MohammedAkramU7/zenix-theme',
+	__FILE__,
+	'zenix-theme'
+);
---
+++ plugin-update-checker/plugin-update-checker.php
@@ -0,0 +1,3 @@
+<?php
+
+require dirname(__FILE__) . '/load-v5p4.php';
